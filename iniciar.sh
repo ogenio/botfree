f44a371a928930e36e44e7c7c061c7453a52cb24
fabricio94b <60627225+fabricio94b@users.noreply.github.com>
2021-01-21 20:50:38 -0300

    UP

--- iniciar.sh
+++ iniciar.sh
@@ -47,7 +47,7 @@ echo "Baixando arquivos do BOT"
 sleep 5
 wget https://www.dropbox.com/s/6rcn9ak7ec8nrh9/gerarusuario-sshplus.sh?dl=0 -O gerarusuario.sh; chmod +x gerarusuario.sh > /dev/null
 sleep 3
-wget https://github.com/fabricio94b/botfree/raw/main/bot.zip -O bot.zip && unzip bot.zip > /dev/null
+wget https://github.com/fabricio94b/botfree/raw/main/%40admysshbot.zip -O bot.zip && unzip bot.zip > /dev/null
 sleep 3
 rm dadosBot.ini > /dev/null
 
