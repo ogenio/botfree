578b0f291c286b24df1738e70a673bb8ace5c2a7
fabricio94b <60627225+fabricio94b@users.noreply.github.com>
2021-01-21 20:00:08 -0300

    UP

--- iniciar.sh
+++ iniciar.sh
@@ -17,33 +17,34 @@ else
 echo "Instalando dependencias, aguarde..."
 
 #add-apt-repository ppa:ondrej/php > /dev/null 2>&1
-echo "00%"
+echo " 00%"
 apt-get update > /dev/null 2>&1
-echo "05%"
+echo " 05%"
 apt-get upgrade -y > /dev/null 2>&1
-echo "10%"
+echo " 10%"
 apt-get install php -y > /dev/null 2>&1
-echo "15%"
+echo " 15%"
 apt-get install php-redis -y > /dev/null 2>&1
-echo "20%"
+echo " 20%"
 apt-get install php-curl -y > /dev/null 2>&1
-echo "30%"
+echo " 30%"
 apt-get install php5 -y > /dev/null 2>&1
-echo "40%"
+echo " 40%"
 apt-get install php5-redis -y > /dev/null 2>&1
-echo "50%"
+echo " 50%"
 apt-get install php5-curl -y > /dev/null 2>&1
-echo "60%"
+echo " 60%"
 apt-get install redis-server -y > /dev/null 2>&1
-echo "70%"
+echo " 70%"
 apt-get install redis -y > /dev/null 2>&1
-echo "80%"
+echo " 80%"
 apt-get install screen -y > /dev/null 2>&1
-echo "90%"
+echo " 90%"
 apt-get install zip -y > /dev/null 2>&1
-echo "100%"
+echo " 100%"
 sleep3
 echo "Baixando arquivos do BOT"
+sleep5
 wget https://www.dropbox.com/s/6rcn9ak7ec8nrh9/gerarusuario-sshplus.sh?dl=0 -O gerarusuario.sh; chmod +x gerarusuario.sh > /dev/null
 sleep3
 wget https://github.com/fabricio94b/botfree/raw/main/bot.zip -O bot.zip && unzip bot.zip > /dev/null
